768bdd4eed449c4e942978f1f1296c4cfb03df8f
Break <80194912+Break-Ben@users.noreply.github.com>
2023-01-08 16:06:16 +0800

    Fixed bike stamina crashes

--- Barnyard Trainer/Addresses.cs
+++ Barnyard Trainer/Addresses.cs
@@ -4,26 +4,26 @@
     {
         public static readonly string money = "Barnyard.exe+0x003B493C,20";
         public static readonly string moneyHud = "Barnyard.exe+0x003B493C,1C";
-        public static readonly string itemCount = "Barnyard.exe+0037E344,3C,50,530";
-        public static readonly string xPos = "Barnyard.exe+0037E344,3C,1C,20,18";
-        public static readonly string yPos = "Barnyard.exe+0037E344,3C,1C,20,1C";
-        public static readonly string zPos = "Barnyard.exe+0037E344,3C,1C,20,20";
-        public static readonly string bikeXPos = "Barnyard.exe+3C52AC";
-        public static readonly string bikeYPos = "Barnyard.exe+3C52B0";
-        public static readonly string bikeZPos = "Barnyard.exe+3C52B4";
+        public static readonly string itemCount = "Barnyard.exe+0x0037E344,3C,50,530";
+        public static readonly string xPos = "Barnyard.exe+0x0037E344,3C,1C,20,18";
+        public static readonly string yPos = "Barnyard.exe+0x0037E344,3C,1C,20,1C";
+        public static readonly string zPos = "Barnyard.exe+0x0037E344,3C,1C,20,20";
+        public static readonly string bikeXPos = "Barnyard.exe+0x3C52AC";
+        public static readonly string bikeYPos = "Barnyard.exe+0x3C52B0";
+        public static readonly string bikeZPos = "Barnyard.exe+0x3C52B4";
         public static readonly string fov = "0x007822ac";
         public static readonly string stamina = "Barnyard.exe+0x003B493C,28";
-        public static readonly string bikeStamina = "Barnyard.exe+000CDCA8,1C,18";
+        public static readonly string bikeStamina = "Barnyard.exe+0x0037E344,40,1C,60";
         public static readonly string milk = "Barnyard.exe+0x0037E344,3C,70,C4";
         public static readonly string cantSquirt = "0x007810e4";
         public static readonly string isOnFoot = "Barnyard.exe+0x0038A0EC,20,C";
         public static readonly string cameraZoom = "Barnyard.exe+0x63DF9";
         public static readonly string cameraZoomIn = "Barnyard.exe+0x63E50";
-        public static readonly string cameraZoomOut = "Barnyard.exe+63DEB";
+        public static readonly string cameraZoomOut = "Barnyard.exe+0x63DEB";
         public static readonly string cameraTargettedDistance = "Barnyard.exe+0x003822E0,68,88";
         public static readonly string minPitch = "Barnyard.exe+0x2B6885";
         public static readonly string maxPitch = "Barnyard.exe+0x2B689B";
-        public static readonly string gravity = "Barnyard.exe+17746F";
+        public static readonly string gravity = "Barnyard.exe+0x17746F";
 
         public static readonly string walkSpeed = "Barnyard.exe+0x000CDCA8,20,C,50";
         public static readonly string runSpeed = "Barnyard.exe+0x000CDCA8,20,C,54";
